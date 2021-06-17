e2900fef7731b53483796e104f995fb352d1ed45
Tay Jun Jie <jjat1987@gmail.com>
2021-06-17 13:31:44 +0800

    Give option to name output file

--- hash_nric.sh
+++ hash_nric.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 set -e
 
-LOGFILE=$1
+# Create tempfile
 TEMPFILE=$(mktemp)
 
 # Make NRICs upper case
-sed -E 's/[a-zA-Z][0-9]{7}[a-zA-Z]/\U&/g' $LOGFILE > $TEMPFILE
+sed 's/[a-zA-Z][0-9]\{7\}[a-zA-Z]/\U&/g' $1 > $TEMPFILE
 
 # Extract unique NRICs
-NRICS=$(grep -Eio '[a-zA-Z][0-9]{7}[a-zA-Z]' $TEMPFILE \
-    | sort \
-    | uniq)
+NRICS=$(grep -o '[A-Z][0-9]\{7\}[A-Z]' $TEMPFILE | sort -u)
 
 # Hash each NRIC
-for NRIC in $NRICS;
+for NRIC in $NRICS
     do
         HASH=$(echo -n $NRIC | md5sum | awk '{print $1}')
         sed -i "s/$NRIC/$HASH/g" $TEMPFILE
     done
 
-# Move file to present directory
-mv $TEMPFILE $PWD/"hashed_"$LOGFILE
+# Move file to location of input file
+if [ -z $2 ] 
+    then mv $TEMPFILE $(dirname $1)/"hashed_"$1
+else
+    mv $TEMPFILE $(dirname $1)/$2
+fi
